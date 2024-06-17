127de245de258e37594cd8bcb6f76e8ee0514980
kingsleywang1984 <95102932+kingsleywang1984@users.noreply.github.com>
2024-06-18 10:55:55 +1200

    add demo for making tx using custodial account (#2)
    * add demo for making tx using custodial account
    
    * remove some of the debug code
    
    * add error handling if user rejects to sign
    
    * add doc to explain how to send tx with custodial account

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+import { either as E } from 'fp-ts';
+import * as t from 'io-ts';
+
+export const signMessageType = t.strict({
+  id: t.string,
+  tag: t.string,
+  payload: t.strict({
+    response: t.strict({
+      signature: t.string,
+    }),
+    tag: t.string,
+  }),
+});
+
+export type signMessageType = t.TypeOf<typeof signMessageType>;
+
+export const signMessageErrorType = t.strict({
+  id: t.string,
+  tag: t.string,
+  payload: t.strict({
+    error: t.strict({
+      error: t.strict({
+        code: t.string,
+      }),
+      tag: t.string,
+    }),
+    tag: t.string,
+  }),
+});
+
+export type signMessageErrorType = t.TypeOf<typeof signMessageErrorType>;
