f4166cda3a66b657ab1b200cb85db9250afe9294
Darpan <darpan@sylo.io>
2024-07-26 14:00:47 +1200

    dev

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
-export const clientId = 'N8LvGuOdY5CeduNUGdUwB'; // This is a test Client ID, preferably use your own
+export const clientId = 'BWrQm0eONdvU4fSICnsJs'; // This is a test Client ID, preferably use your own
 export const accessToken = '80rEBd2wrPkd4KZg33JQGFN0ILK-_bry5GWSjtadJJL'; // This is a test /manageclients Access Token, preferably use your own
 export const redirectUri = 'http://localhost:3000/callback'; // Ensure this matches the redirect_uri defined on /manageclients
 
-export const identityProviderUri = 'https://login.futureverse.cloud'; // .dev -> DEV, .cloud -> STAGING, .app -> PRODUCTION
+export const identityProviderUri = 'https://login.futureverse.dev'; // .dev -> DEV, .cloud -> STAGING, .app -> PRODUCTION
 
 export const authorizationEndpoint = `${identityProviderUri}/auth`;
 export const tokenEndpoint = `${identityProviderUri}/token`;
 
-export const custodialSignerUrl = `https://signer.futureverse.cloud`;
+export const custodialSignerUrl = `https://signer.futureverse.dev`;
 
 export const alchemyJsonRpcProviderUrl = `https://rpc.sepolia.org/`; // add your onw alchemy json rpc provider url
 
