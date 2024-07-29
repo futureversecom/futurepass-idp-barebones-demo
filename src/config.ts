abc6ff11734c192424d8a36c0969727575f16f55
Darpan Patil <46893851+developer-darpan@users.noreply.github.com>
2024-07-29 12:40:57 +1200

    update clientId (#7)

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-export const clientId = 'N8LvGuOdY5CeduNUGdUwB'; // This is a test Client ID, preferably use your own
-export const accessToken = '80rEBd2wrPkd4KZg33JQGFN0ILK-_bry5GWSjtadJJL'; // This is a test /manageclients Access Token, preferably use your own
+export const clientId = '8XPY4Vnc6BBn_4XNBYk0P'; // This is a test Client ID, preferably use your own
+export const accessToken = 'G-Y5OwG_2NDRLFTpLpyjX92WyLMia2t0PcmPboGeMqi'; // This is a test /manageclients Access Token, preferably use your own
 export const redirectUri = 'http://localhost:3000/callback'; // Ensure this matches the redirect_uri defined on /manageclients
 
 export const identityProviderUri = 'https://login.futureverse.cloud'; // .dev -> DEV, .cloud -> STAGING, .app -> PRODUCTION
